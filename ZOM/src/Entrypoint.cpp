dbb9a0ae3d2f63a0aefa6cae71ab75e804d75962
ondranedo <nedojedly.o@gmail.com>
2022-10-02 20:06:08 +0200

    +platform check

--- ZOM/src/Entrypoint.cpp
+++ ZOM/src/Entrypoint.cpp
@@ -16,6 +16,21 @@ int main(int argc, char** argv)
 	ZOM_CLIENT_INFO("Distribute build");
 #endif
 
+#ifdef ZOM_WINDOWS
+	ZOM_CLIENT_INFO("Building on Windows");
+
+#elif  ZOM_LINUX
+	ZOM_CLIENT_CRITICAL("Building on Linux, not supported... quitting.");
+	return -1;
+
+#elif ZOM_MACOS
+	ZOM_CLIENT_CRITICAL("Building on MacOS, not supported... quitting.");
+	return -1;
+#else
+	ZOM_CLIENT_CRITICAL("Building on unkonw device... quitting.");
+	return -1;
+#endif
+
 	ZOM::ZOMGameEngine* app = createApp();
 
 	app->run();

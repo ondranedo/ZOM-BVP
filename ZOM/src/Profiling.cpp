0e751ad4bf7e393e1e3c643d0d93338c1a2dcfbd
ondranedo <nedojedly.o@gmail.com>
2022-10-22 23:59:51 +0200

    fix

--- ZOM/src/Profiling.cpp
+++ ZOM/src/Profiling.cpp
@@ -29,32 +29,40 @@ namespace ZOM {
 	void ChromeProfiling::init()
 	{
 		m_Path = Config::profilePath();
-		m_File.open(m_Path, std::ios::out);
-		header();
+		if (Config::profile())
+		{
+			m_File.open(m_Path, std::ios::out);
+			header();
+		}
 	}
 
 	void ChromeProfiling::release()
 	{
-		footer();
-		m_File.close();
+		if (m_File.is_open())
+		{
+			footer();
+			m_File.close();
+		}
 	}
 
 	void ChromeProfiling::profile(const profileData& data)
 	{
-		if (profileCount++ > 0)
-			m_File << ",";
+		if (m_File.is_open()) {
+			if (profileCount++ > 0)
+				m_File << ",";
 
-		m_File << "{\n";
-		m_File << "	    \"name\": \"" << data.name << "\",\n";
-		m_File << "		\"cat\" : \"function\",\n";
-		m_File << "		\"ph\"  : \"X\",\n";
-		m_File << "		\"ts\"  : " << data.start << ",\n";
-		m_File << "		\"dur\" : " << (data.end - data.start) << ",\n";
-		m_File << "		\"pid\" : 0,\n";
-		m_File << "		\"tid\" : 0,\n";
-		m_File << "		\"args\": {}\n";
-		m_File << "}\n";
-		m_File.flush();
+			m_File << "{\n";
+			m_File << "	    \"name\": \"" << data.name << "\",\n";
+			m_File << "		\"cat\" : \"function\",\n";
+			m_File << "		\"ph\"  : \"X\",\n";
+			m_File << "		\"ts\"  : " << data.start << ",\n";
+			m_File << "		\"dur\" : " << (data.end - data.start) << ",\n";
+			m_File << "		\"pid\" : 0,\n";
+			m_File << "		\"tid\" : 0,\n";
+			m_File << "		\"args\": {}\n";
+			m_File << "}\n";
+			m_File.flush();
+		}
 	}
 
 	void ChromeProfiling::header()

4f8758312e398baa685ea9fc965e5fc1877066ca
ondranedo <nedojedly.o@gmail.com>
2022-10-24 00:00:08 +0200

    - mat1 form complex + InShaderDT to string

--- ZOM/src/codes/ComplexDataType.cpp
+++ ZOM/src/codes/ComplexDataType.cpp
@@ -14,7 +14,6 @@ size_t ZOMInShaderDataTypeSize(InShaderDataType isdt)
 	case ZOM::InShaderDataType::VecI2: return ZOMDataTypeSize(dataType::Int4) * 2;
 	case ZOM::InShaderDataType::VecI3: return ZOMDataTypeSize(dataType::Int4) * 3;
 	case ZOM::InShaderDataType::VecI4: return ZOMDataTypeSize(dataType::Int4) * 4;
-	case ZOM::InShaderDataType::Mat1:  return ZOMDataTypeSize(dataType::F4) * 1 * 1;
 	case ZOM::InShaderDataType::Mat2:  return ZOMDataTypeSize(dataType::F4) * 2 * 2;
 	case ZOM::InShaderDataType::Mat3:  return ZOMDataTypeSize(dataType::F4) * 3 * 3;
 	case ZOM::InShaderDataType::Mat4:  return ZOMDataTypeSize(dataType::F4) * 4 * 4;
@@ -38,7 +37,6 @@ size_t ZOMInShaderDataTypeComponentCount(InShaderDataType isdt)
 	case ZOM::InShaderDataType::VecI2: return 2;
 	case ZOM::InShaderDataType::VecI3: return 3;
 	case ZOM::InShaderDataType::VecI4: return 4;
-	case ZOM::InShaderDataType::Mat1:  return 1 * 1;
 	case ZOM::InShaderDataType::Mat2:  return 2 * 2;
 	case ZOM::InShaderDataType::Mat3:  return 3 * 3;
 	case ZOM::InShaderDataType::Mat4:  return 4 * 4;
@@ -47,6 +45,27 @@ size_t ZOMInShaderDataTypeComponentCount(InShaderDataType isdt)
 
 	ZOM_ERROR("Unkown data type passed in to fucntion: ZOMInShaderDataTypeComponentCount(ZOM::InShaderDataType type)");
 	return 0;
+}
+
+extern InShaderDataType strToZOMInShaderDataType(const std::string& str)
+{
+	
+	if (str == "Null")  return ZOM::InShaderDataType::Null;
+	if (str == "float") return ZOM::InShaderDataType::VecF1;
+	if (str == "vec2")  return ZOM::InShaderDataType::VecF2;
+	if (str == "vec3")  return ZOM::InShaderDataType::VecF3;
+	if (str == "vec4")  return ZOM::InShaderDataType::VecF4;
+	if (str == "int")   return ZOM::InShaderDataType::VecI1;
+	if (str == "ivec2") return ZOM::InShaderDataType::VecI2;
+	if (str == "ivec3") return ZOM::InShaderDataType::VecI3;
+	if (str == "ivec4") return ZOM::InShaderDataType::VecI4;
+	if (str == "mat2")  return ZOM::InShaderDataType::Mat2;
+	if (str == "mat3")  return ZOM::InShaderDataType::Mat3;
+	if (str == "mat4")  return ZOM::InShaderDataType::Mat4;
+	if (str == "bool")  return ZOM::InShaderDataType::Bool;
+
+	ZOM_ERROR("Unkown data type passed in to fucntion: ZOMInShaderDataTypeComponentCount(ZOM::InShaderDataType type)");
+	return ZOM::InShaderDataType::Null;
 }
 }
 
--- ZOM/src/codes/ComplexDataType.h
+++ ZOM/src/codes/ComplexDataType.h
@@ -5,9 +5,10 @@ namespace ZOM {
 		Null, 
 		VecF1, VecF2, VecF3, VecF4,
 		VecI1, VecI2, VecI3, VecI4,
-		Mat1, Mat2, Mat3, Mat4, Bool
+	    Mat2, Mat3, Mat4, Bool
 	};
 
 	extern size_t ZOMInShaderDataTypeSize(InShaderDataType isdt);
 	extern size_t ZOMInShaderDataTypeComponentCount(InShaderDataType isdt);
+	extern InShaderDataType strToZOMInShaderDataType(const std::string& str);
 }
